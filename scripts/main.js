df54600e685bf74d536d1af7d6d34fb028389606
rpradosilva <contato@rprado.design>
2021-10-23 19:00:42 -0300

    refactor: update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,16 @@
-const socialLinks = {
-  youtube: "UCngcgGLbSjDUO-H2p5j43DA",
-  instagram: "rpradosilva",
-  facebook: "",
-  twitter: "rpradosilva",
-};
-
-function loadSocialLinks(socialLinks) {
-  let socialLinks = document.querySelectorAll(".card__social-link a");
+function insertSocialLinks() {
+  const social = {
+    youtube: "UCngcgGLbSjDUO-H2p5j43DA",
+    instagram: "rpradosilva",
+    facebook: "",
+    twitter: "rpradosilva",
+  };
 
   const youtubeUrl = "https://www.youtube.com/channel/"; //UCngcgGLbSjDUO-H2p5j43DA
   const instagramUrl = "https://www.instagram.com/"; //rpradosilva
   const facebookUrl = "https://www.facebook.com/"; // ??
   const twitterUrl = "https://twitter.com/"; //rpradosilva
+  let socialLinks = document.querySelectorAll(".card__social-link a");
 
   for (const link of socialLinks) {
   }
